fd0ffc4f8dde2ce68350e13c8c7d6e9f88782934
MikeMike39 <Mfonseca294@gmail.com>
2021-04-03 08:40:16 -0400

    Done with assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+//"Has a functional expression called divide" That "Divides 2000 by 100"
+const divide = (d) => 2000 / 100;
+//"Has an arrow function called square" That "Square arrow function multiplies a number times itself" 
+const square = (m) => 2 * 2;
+//"Has an arrow function called add" That "Adds an arrow function that takes two parameters and adds them together"
+const add = (a) => 3 + 4;
